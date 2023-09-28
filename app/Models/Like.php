1baa6209d731b60b652163b5ac1861894aa4ab5b
AhorIsaac <ahorisaac.ai@gmail.com>
2023-09-28 14:08:37 +0100

    chore: create eloquent relations among all the models

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Like extends Model
 {
     use HasFactory;
+
+    protected $table = "likes";
+
+    protected $fillable = ["event_id", "user_id"];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
